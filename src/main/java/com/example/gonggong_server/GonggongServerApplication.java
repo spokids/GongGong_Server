7b297ff42411a7546dda0429fdef00a6b12b9698
pkl0912 <pklpkl0912@gmail.com>
2024-11-13 08:07:27 +0900

    first init

---
+++ src/main/java/com/example/gonggong_server/GonggongServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gonggong_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonggongServerApplication.class, args);
+    }
+
+}
