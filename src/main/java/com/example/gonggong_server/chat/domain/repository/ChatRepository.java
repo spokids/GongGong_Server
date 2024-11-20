a16c22965332ece1cacd3151b43b78cea0123598
pkl0912 <pklpkl0912@gmail.com>
2024-11-21 01:22:12 +0900

    feat: 회원탈퇴 기능 개발

--- src/main/java/com/example/gonggong_server/chat/domain/repository/ChatRepository.java
+++ src/main/java/com/example/gonggong_server/chat/domain/repository/ChatRepository.java
@@ -1,4 +1,7 @@
 package com.example.gonggong_server.chat.domain.repository;
 
+import java.util.List;
+
 public interface ChatRepository {
+    void deleteByChatroomIds(List<Long> chatroomIds);
 }
--- src/main/java/com/example/gonggong_server/user/application/service/UserService.java
+++ src/main/java/com/example/gonggong_server/user/application/service/UserService.java
@@ -1,11 +1,47 @@
 package com.example.gonggong_server.user.application.service;
 
+import com.example.gonggong_server.auth.exception.AuthException;
+import com.example.gonggong_server.chat.domain.repository.ChatRepository;
+import com.example.gonggong_server.chatroom.domain.repository.ChatRoomRepository;
+import com.example.gonggong_server.global.status.ErrorStatus;
+import com.example.gonggong_server.review.domain.repository.ReviewRepository;
+import com.example.gonggong_server.scrap.domain.repository.ScrapRepository;
+import com.example.gonggong_server.user.domain.entity.User;
+import com.example.gonggong_server.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final ScrapRepository scrapRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatRepository chatRepository;
+    private final ReviewRepository reviewRepository;
 
+    @Transactional
+    public void deleteUser(String userInputId) {
+        System.out.println(userInputId);
+        User user = findUserById(userInputId);
+        Long userId = user.getUserId();
 
+        deleteUserRelatedData(userId);
+
+        userRepository.delete(user);
+    }
+    private User findUserById(String userInputId) {
+        return userRepository.findByUserInputId(userInputId)
+                .orElseThrow(() -> new AuthException(ErrorStatus.USER_NOT_FOUND));
+    }
+    private void deleteUserRelatedData(Long userId) {
+        scrapRepository.deleteByUserId(userId);
+        reviewRepository.deleteByUserId(userId);
+        List<Long> chatroomIds = chatRoomRepository.findAllIdsByUserId(userId);
+        chatRepository.deleteByChatroomIds(chatroomIds);
+        chatRoomRepository.deleteByUserId(userId);
+    }
 }
