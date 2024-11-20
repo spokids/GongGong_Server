e210a70368bfc378d1750322999a82770883b87c
pkl0912 <pklpkl0912@gmail.com>
2024-11-21 02:38:58 +0900

    feat: 스크랩 리스트 조회 기능 개발

---
+++ src/main/java/com/example/gonggong_server/scrap/application/response/ScrapListResponseDTO.java
@@ -0,0 +1,74 @@
+package com.example.gonggong_server.scrap.application.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScrapListResponseDTO{
+    private String nickName;
+    private String userInputId;
+    private List<ScrapProgramDTO> scraps;
+    private int totalPage;
+    private int currentPage;
+
+    public static ScrapListResponseDTO of(
+            String nickName,
+            String userInputId,
+            List<ScrapProgramDTO> scraps,
+            int totalPage,
+            int currentPage
+    ) {
+        return ScrapListResponseDTO.builder()
+                .nickName(nickName)
+                .userInputId(userInputId)
+                .scraps(scraps)
+                .totalPage(totalPage)
+                .currentPage(currentPage)
+                .build();
+    }
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ScrapProgramDTO {
+        private Long programId;
+        private String programType;
+        private String programName;
+        private String facilityName;
+        private String programAge;
+        private String programDate;
+
+        public static ScrapListResponseDTO.ScrapProgramDTO of(Long programId, String programType,String programName, String facilityName,
+                                                           int startAge, int endAge, LocalDate startDate, LocalDate endDate) {
+            return ScrapProgramDTO.builder()
+                    .programId(programId)
+                    .programType(programType)
+                    .programName(programName)
+                    .facilityName(facilityName)
+                    .programAge(formatProgramAge(startAge, endAge))
+                    .programDate(formatProgramDate(startDate, endDate))
+                    .build();
+        }
+
+        // 날짜를 "yy.MM.dd-yy.MM.dd" 형식으로 변환
+        private static String formatProgramDate(LocalDate startDate, LocalDate endDate) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy.MM.dd");
+            return startDate.format(formatter) + " - " + endDate.format(formatter);
+        }
+
+        // 나이를 "만 x세~만 y세" 형식으로 변환
+        private static String formatProgramAge(int startAge, int endAge) {
+            return "만 " + startAge + "세 ~ 만 " + endAge + "세";
+        }
+    }
+}
--- src/main/java/com/example/gonggong_server/scrap/application/service/ScrapService.java
+++ src/main/java/com/example/gonggong_server/scrap/application/service/ScrapService.java
@@ -2,14 +2,23 @@ package com.example.gonggong_server.scrap.application.service;
 
 import com.example.gonggong_server.auth.exception.AuthException;
 import com.example.gonggong_server.global.status.ErrorStatus;
+import com.example.gonggong_server.program.domain.entity.Program;
+import com.example.gonggong_server.program.domain.repository.ProgramRepository;
+import com.example.gonggong_server.program.exception.ProgramException;
+import com.example.gonggong_server.scrap.application.response.ScrapListResponseDTO;
 import com.example.gonggong_server.scrap.domain.entity.Scrap;
 import com.example.gonggong_server.scrap.domain.repository.ScrapRepository;
 import com.example.gonggong_server.scrap.exception.ScrapException;
 import com.example.gonggong_server.user.domain.entity.User;
 import com.example.gonggong_server.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +26,7 @@ public class ScrapService {
 
     private final UserRepository userRepository;
     private final ScrapRepository scrapRepository;
+    private final ProgramRepository programRepository;
 
     @Transactional
     public void scrapProgram(String userInputId, Long programId) {
@@ -53,5 +63,45 @@ public class ScrapService {
         if (deletedCount == 0) {
             throw new ScrapException(ErrorStatus.NOT_SCRAPPED);
         }
+    }
+    public ScrapListResponseDTO getScrapList(String userInputId, int pageSize, int pageIndex) {
+        User user = findUserById(userInputId);
+
+        Pageable pageable = PageRequest.of(pageIndex - 1, pageSize);
+        Page<Scrap> scraps = scrapRepository.findScraps(user.getUserId(), pageable);
+
+        List<ScrapListResponseDTO.ScrapProgramDTO> scrapPrograms = convertScrapsToDTOs(scraps.getContent());
+
+        return buildScrapListResponse(user, scrapPrograms, scraps);
+    }
+
+    private List<ScrapListResponseDTO.ScrapProgramDTO> convertScrapsToDTOs(List<Scrap> scraps) {
+        return scraps.stream()
+                .map(scrap -> {
+                    Program program = programRepository.findByProgramId(scrap.getProgramId())
+                            .orElseThrow(() -> new ProgramException(ErrorStatus.PROGRAM_NOT_EXIST));
+
+                    return ScrapListResponseDTO.ScrapProgramDTO.of(
+                            program.getProgramId(),
+                            program.getType(),
+                            program.getProgramName(),
+                            program.getFacultyName(),
+                            program.getStartAge(),
+                            program.getEndAge(),
+                            program.getProgramStartDate(),
+                            program.getProgramEndDate()
+                    );
+                })
+                .toList();
+    }
+
+    private ScrapListResponseDTO buildScrapListResponse(User user, List<ScrapListResponseDTO.ScrapProgramDTO> scrapPrograms, Page<Scrap> scraps) {
+        return ScrapListResponseDTO.of(
+                user.getNickName(),
+                user.getUserInputId(),
+                scrapPrograms,
+                scraps.getTotalPages(),
+                scraps.getNumber() + 1 
+        );
     }
 }
