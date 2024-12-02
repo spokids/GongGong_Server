65f880bd53f7753ba9633f8aacdaa22d9f273047
pkl0912 <pklpkl0912@gmail.com>
2024-12-02 13:23:39 +0900

    fix: cors 수정

--- src/main/java/com/example/gonggong_server/global/config/WebConfig.java
+++ src/main/java/com/example/gonggong_server/global/config/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
-                .allowedHeaders("Authorization", "Content-Type")
+                .allowedOriginPatterns("http://localhost:5173", "http://localhost:8080", "http://localhost:8081", "https://api.spokids.shop", "http://api.spokids.shop", "http://spokids.shop", "https://spokids.shop")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
+                .allowedHeaders("Authorization", "Content-Type", "Access-Control-Request-Headers")
                 .allowCredentials(true)
                 .maxAge(3000);
     }
